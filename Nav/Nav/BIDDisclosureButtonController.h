1a738b03436cf78424fa855b2ce65265aa474c39
=Shaliemar Abucay <shlabucay@gmail.com>
2013-09-16 16:31:11 +0800

    Created the First Sub-Controller : The Disclosure Button View.

---
+++ Nav/Nav/BIDDisclosureButtonController.h
@@ -0,0 +1,13 @@
+//
+//  BIDDisclosureButtonController.h
+//  Nav
+//
+//  Created by Maria Alice C. Lim on 9/16/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import "BIDSecondLevelViewController.h"
+
+@interface BIDDisclosureButtonController : BIDSecondLevelViewController
+
+@end
