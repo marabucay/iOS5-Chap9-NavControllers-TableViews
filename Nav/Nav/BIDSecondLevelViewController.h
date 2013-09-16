f9b6ab31d876422f7065b6259c5e000f66317ae0
=Shaliemar Abucay <shlabucay@gmail.com>
2013-09-16 16:17:17 +0800

    Created Second Level Controller.

---
+++ Nav/Nav/BIDSecondLevelViewController.h
@@ -0,0 +1,13 @@
+//
+//  BIDSecondLevelViewController.h
+//  Nav
+//
+//  Created by Maria Alice C. Lim on 9/16/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BIDSecondLevelViewController : UITableViewController
+
+@end
---
+++ Nav/Nav/BIDSecondLevelViewController.m
@@ -0,0 +1,13 @@
+//
+//  BIDSecondLevelViewController.m
+//  Nav
+//
+//  Created by Maria Alice C. Lim on 9/16/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import "BIDSecondLevelViewController.h"
+
+@implementation BIDSecondLevelViewController
+
+@end
