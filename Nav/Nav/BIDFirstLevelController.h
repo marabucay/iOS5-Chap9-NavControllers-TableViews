071b1fb6c4aa6291a1eae0604d7c4bc21ec581fa
=Shaliemar Abucay <shlabucay@gmail.com>
2013-09-16 15:51:57 +0800

    Created the top level view controller.

---
+++ Nav/Nav/BIDFirstLevelController.h
@@ -0,0 +1,13 @@
+//
+//  BIDFirstLevelController.h
+//  Nav
+//
+//  Created by Maria Alice C. Lim on 9/16/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BIDFirstLevelController : UITableViewCell
+
+@end
---
+++ Nav/Nav/BIDFirstLevelController.m
@@ -0,0 +1,29 @@
+//
+//  BIDFirstLevelController.m
+//  Nav
+//
+//  Created by Maria Alice C. Lim on 9/16/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import "BIDFirstLevelController.h"
+
+@implementation BIDFirstLevelController
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
