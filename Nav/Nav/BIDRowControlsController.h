091220e367b693a1943fa872fda52be463d68d83
=Shaliemar Abucay <shlabucay@gmail.com>
2013-09-16 17:32:53 +0800

    Created the Third Subcontroller: Controls on Table Rows.

---
+++ Nav/Nav/BIDRowControlsController.h
@@ -0,0 +1,13 @@
+//
+//  BIDRowControlsController.h
+//  Nav
+//
+//  Created by Maria Alice C. Lim on 9/16/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import "BIDSecondLevelViewController.h"
+
+@interface BIDRowControlsController : BIDSecondLevelViewController
+
+@end
---
+++ Nav/Nav/BIDRowControlsController.m
@@ -0,0 +1,13 @@
+//
+//  BIDRowControlsController.m
+//  Nav
+//
+//  Created by Maria Alice C. Lim on 9/16/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import "BIDRowControlsController.h"
+
+@implementation BIDRowControlsController
+
+@end
